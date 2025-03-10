7fe553e6efddd7082841bca94faaf8f12c165b3a
vartakdipeesha <164518650+vartakdipeesha@users.noreply.github.com>
2025-03-10 21:26:27 +0530

    Delete doctor-service/appointment-service directory

--- doctor-service/appointment-service/src/main/java/com/example/repository/AppointmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.repository;
-
-import com.example.model.Appointment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
-}
--- doctor-service/appointment-service/target/classes/data.sql
+++
@@ -1,8 +0,0 @@
--- data.sql
-
-INSERT INTO appointment (appointment_date, time, doctor_name, patient_name, status)
-VALUES
-    ('2025-03-05', '10:00:00', 'Dr. Smith', 'Dipeesha', 'Scheduled'),
-    ('2025-03-06', '11:30:00', 'Dr. Brown', 'Dhruvi ', 'Completed'),
-
-    ('2025-03-07', '14:00:00', 'Dr. Green', 'Nitya', 'Cancelled');
